b6ac37e968a2d6e0ec95d304f4d42446369baf57
jezrsmith <jez.smith@live.com>
2019-05-23 12:06:21 +0100

    Improved the form test app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,15 @@ import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angula
 })
 export class AppComponent {
 
-  // data: any = { formRef: 'Partner Request.PartnerRequest.casedata.default', formData: JSON.parse('{"RequestDescription_v1":"Please close my account","CaseMetrics_v1":{"Priority_v1":1,"DueDate_v1":"2019-05-09","Owner_v1":"Jon West","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"0101001","PartnerContactEmail_v1":"jon@tibco.com"},"state":"Created","PartnerReference_v1":"99000123","RequestType_v1":"Product Recall","CaseId_v1":"PNR-62"}') };
+  ref: string = 'Partner Request.PartnerRequest.action.Update Request Metrics';
+  data: any;
+  dataJSON = '{"PartnerRequest": {"CaseMetrics_v1":{"Priority_v1":4,"DueDate_v1":"2019-05-22","Owner_v1":"jon@tibco.com","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"55555","PartnerContactEmail_v1":"jon@tibco.com"},"state":"Created","PartnerReference_v1":"99000123","CaseId_v1":"PNR-74","Customer_v1":{"Name_v1":"Jon Werst","CustomerReference_v1":"Ref 1111","CustomerDetailsExtract_v1":"Customer 123","CustomerEmail_v1":"customer@customer.com"},"RequestDetails_v1":{"PartReference_v1":"Part-1","PartName_v1":"Part 1","PartDescription_v1":"A part","OrderReference_v1":"Order 1"},"RequestType_v1":"Account Enquiry"}}';
+  submittedData: string;
+
   // ref = 'Partner Request.PartnerRequest.creator.New Product Notification';
-   ref = 'Partner Request.PartnerRequest.action.Update Request Metrics';
+  // ref = 'Partner Request.PartnerRequest.action.Update Request Metrics';
+
+  // data: any = { formRef: 'Partner Request.PartnerRequest.casedata.default', formData: JSON.parse('{"RequestDescription_v1":"Please close my account","CaseMetrics_v1":{"Priority_v1":1,"DueDate_v1":"2019-05-09","Owner_v1":"Jon West","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"0101001","PartnerContactEmail_v1":"jon@tibco.com"},"state":"Created","PartnerReference_v1":"99000123","RequestType_v1":"Product Recall","CaseId_v1":"PNR-62"}') };
   // data: any = {};
   /* data: any = {
     ProductDetails_v1: 'Hello',
@@ -21,25 +27,24 @@ export class AppComponent {
     }
   }*/
   // data: any = JSON.parse('{"PartnerRequest":{"RequestType_v1":"Packaging Supplies","RequestDescription_v1":"sdsdsd","Customer_v1":{"Name_v1":"asdasdsa","CustomerReference_v1":"asdsa-asda","CustomerDetailsExtract_v1":"sadsadas","CustomerEmail_v1":"sadas@sad.com"},"RequestDetails_v1":{"PartReference_v1":"sdsds","PartName_v1":"asdasdasdasd","PartDescription_v1":"sdsds","OrderReference_v1":"ORD-12121","IssueLog_v1":[{"LogEntry_v1":"sdsds"},{"LogEntry_v1":"dsds"}]},"CaseMetrics_v1":{"Priority_v1":4,"DueDate_v1":"2019-05-30","Owner_v1":"Jon West","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"0101001","PartnerContactEmail_v1":"jon@tibco.com"}}}');
-
   // data: any = JSON.parse('{"PartnerRequest": {"CaseMetrics_v1":{"Priority_v1":4,"DueDate_v1":"2019-05-30","Owner_v1":"Jon West","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"0101001","PartnerContactEmail_v1":"jon@tibco.com"},"state":"Created","PartnerReference_v1":"99000123","CaseId_v1":"PNR-62","Customer_v1":{"Name_v1":"asdasdsa","CustomerReference_v1":"asdsa-asda","CustomerDetailsExtract_v1":"sadsadas","CustomerEmail_v1":"sadas@sad.com"},"RequestDetails_v1":{"PartReference_v1":"sdsds","PartName_v1":"asdasdasdasd","PartDescription_v1":"sdsds","OrderReference_v1":"ORD-12121","IssueLog_v1":[{"LogEntry_v1":"test"},{"LogEntry_v1":"test2"}]},"RequestDescription_v1":"sdsdsd","RequestType_v1":"Rates & Transit Times"}}');
-  data: any = JSON.parse('{"PartnerRequest": {"CaseMetrics_v1":{"Priority_v1":4,"DueDate_v1":"2019-05-22","Owner_v1":"jon@tibco.com","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"55555","PartnerContactEmail_v1":"jon@tibco.com"},"state":"Created","PartnerReference_v1":"99000123","CaseId_v1":"PNR-74","Customer_v1":{"Name_v1":"Jon Werst","CustomerReference_v1":"Ref 1111","CustomerDetailsExtract_v1":"Customer 123","CustomerEmail_v1":"customer@customer.com"},"RequestDetails_v1":{"PartReference_v1":"Part-1","PartName_v1":"Part 1","PartDescription_v1":"A part","OrderReference_v1":"Order 1"},"RequestType_v1":"Account Enquiry"}}');
+  // data: any = JSON.parse('{"PartnerRequest": {"CaseMetrics_v1":{"Priority_v1":4,"DueDate_v1":"2019-05-22","Owner_v1":"jon@tibco.com","RequestOwnerEmail_v1":"jon@tibco.com","RequestOwnerSMS_v1":"55555","PartnerContactEmail_v1":"jon@tibco.com"},"state":"Created","PartnerReference_v1":"99000123","CaseId_v1":"PNR-74","Customer_v1":{"Name_v1":"Jon Werst","CustomerReference_v1":"Ref 1111","CustomerDetailsExtract_v1":"Customer 123","CustomerEmail_v1":"customer@customer.com"},"RequestDetails_v1":{"PartReference_v1":"Part-1","PartName_v1":"Part 1","PartDescription_v1":"A part","OrderReference_v1":"Order 1"},"RequestType_v1":"Account Enquiry"}}');
 
-  data2: any = {
-    ProductDetails_v1: 'Hello2',
-    PartnerRequest: {
-      CaseMetrics_v1: {
-        Priority_v1: 3
-      }
-    }
+
+  handleShowForm = () => {
+    this.submittedData = undefined;
+    this.data = JSON.parse(this.dataJSON);
+  }
+
+  handleReuseData = () => {
+    this.dataJSON = JSON.stringify(this.submittedData);
   }
 
   handleSubmit = (data) => {
     console.log('Form submitted!: ', data);
+    this.submittedData = data;
+    this.data = undefined;
   }
 
-  updateData = () => {
-    this.data = this.data2;
-  }
 
 }
